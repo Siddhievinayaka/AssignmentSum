299c834fa0a488bd41bcf085d2598873589a75b6
Siddhievinayaka <siddhievinayakakharaat5@gmail.com>
2025-11-30 20:36:14 +0530

    Assignment 2

---
+++ AssignmentNo2/Task1.py
@@ -0,0 +1,9 @@
+# Check if a Number is Even or Odd
+
+user_input = int(input("Enter a number: "))
+
+if user_input % 2 == 0:  # Check if the number is divisible by 2 and remainder is 0.
+    print("The number is Even")
+
+else:
+    print("The number is Odd")
---
+++ AssignmentNo2/Task2.py
@@ -0,0 +1,8 @@
+# Sum of Integers from 1 to 50 Using a Loop
+
+result = 0
+for i in range(1, 51):
+    result += i
+
+
+print("\nThe sum of integers from 1 to 50 is:", result)
